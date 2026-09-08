2a517568f69f38030330f6321af307bf598f3f79
nativeelaneeru <ir117737@gmail.com>
2026-09-09 00:34:37 +0530

    Publish Native Elaneeru PWA V10.4

---
+++ customer-web/business/sw.js
@@ -0,0 +1,14 @@
+const CACHE='native-elaneeru-business-v9.6.0';
+const SHELL=['./','./index.html','./v96.html','./config.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin) return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match('./v96.html').then(x=>x||caches.match('./index.html'))));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r})));
+});
