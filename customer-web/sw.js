c50f3cb0d13d553f223484c6b806669f9ed11ec4
nativeelaneeru <ir117737@gmail.com>
2026-09-08 13:33:07 +0530

    Bump B2C PWA cache for soft launch safeguards

--- customer-web/sw.js
+++ customer-web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='native-elaneeru-v9.0.1';
+const CACHE='native-elaneeru-v9.0.2';
 const SHELL=['./','./index.html','./config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
