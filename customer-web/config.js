7ebb6713c71d99084151f337fb70a8b5057c6864
nativeelaneeru <ir117737@gmail.com>
2026-09-09 09:39:39 +0530

    Activate V10.10 final B2C UI layer

--- customer-web/config.js
+++ customer-web/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycby4tO2Y1xWbVv1XqzbIYjuTKOt4XRWPzB9MseMC1x-qc8gbNYPvqvG1z6PuCPGZi5O2/exec',
-  appVersion: '10.9.0-pwa',
+  appVersion: '10.10.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
@@ -10,5 +10,8 @@ window.NEL_CONFIG = {
   deliveryRadiusKm: 3
 };
 if (!window.NEL_RPC_V109) {
-  document.write('<script src="./rpc-v109.js?v=1090"><\/script>');
+  document.write('<script src="./rpc-v109.js?v=1010"><\/script>');
+}
+if (!window.NEL_UI_V110) {
+  document.write('<script src="./ui-v110.js?v=1010"><\/script>');
 }
