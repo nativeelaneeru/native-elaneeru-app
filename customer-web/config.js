b618171f791e6cd099b94fcc3318433677d4edb3
nativeelaneeru <ir117737@gmail.com>
2026-09-09 15:20:54 +0530

    Bump PWA version for PIN flow fix

--- customer-web/config.js
+++ customer-web/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycby4tO2Y1xWbVv1XqzbIYjuTKOt4XRWPzB9MseMC1x-qc8gbNYPvqvG1z6PuCPGZi5O2/exec',
-  appVersion: '10.11.0-pwa',
+  appVersion: '10.12.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
@@ -10,11 +10,11 @@ window.NEL_CONFIG = {
   deliveryRadiusKm: 3
 };
 if (!window.NEL_RPC_V109) {
-  document.write('<script src="./rpc-v109.js?v=1011"><\/script>');
+  document.write('<script src="./rpc-v109.js?v=1012"><\/script>');
 }
 if (!window.NEL_UI_V110) {
-  document.write('<script src="./ui-v110.js?v=1011"><\/script>');
+  document.write('<script src="./ui-v110.js?v=1012"><\/script>');
 }
 if (!window.NEL_UI_V111) {
-  document.write('<script src="./ui-v111.js?v=1011"><\/script>');
+  document.write('<script src="./ui-v111.js?v=1012"><\/script>');
 }
