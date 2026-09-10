5522e56801b48665fe116ae99f0cc38ffefbe7da
nativeelaneeru <ir117737@gmail.com>
2026-09-10 16:23:50 +0530

    Preserve verified B2C compatibility version

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.33.0-pwa',
+  appVersion: '10.31.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
