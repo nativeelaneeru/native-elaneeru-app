a46afd924244a17835d7ca812c43f00a11b2107a
nativeelaneeru <ir117737@gmail.com>
2026-09-10 12:08:43 +0530

    Load B2C order recovery v10.25

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.24.0-pwa',
+  appVersion: '10.25.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
@@ -10,21 +10,22 @@ window.NEL_CONFIG = {
   deliveryRadiusKm: 3
 };
 
-/* Use the B2C shell's supported POST bridge. The legacy JSONP transport
- * cannot load the current product and banner payload from this deployment.
- */
+/* Use the B2C shell's supported POST bridge. */
 window.NEL_RPC_V109 = true;
 
 if (!window.NEL_UI_V110) {
-  document.write('<script src="./ui-v110.js?v=10240"></script>');
+  document.write('<script src="./ui-v110.js?v=10250"></script>');
 }
 if (!window.NEL_UI_V111) {
-  document.write('<script src="./ui-v111.js?v=10240"></script>');
+  document.write('<script src="./ui-v111.js?v=10250"></script>');
 }
 if (!window.NEL_UI_V112) {
-  document.write('<script src="./ui-v112.js?v=10240"></script>');
+  document.write('<script src="./ui-v112.js?v=10250"></script>');
 }
 if (!window.NEL_UI_V113) {
-  document.write('<script src="./ui-v113.js?v=10240"></script>');
+  document.write('<script src="./ui-v113.js?v=10250"></script>');
 }
-document.write('<script src="./separate-links.js?v=10240"></script>');
+if (!window.NEL_UI_V125) {
+  document.write('<script src="./ui-v125.js?v=10250"></script>');
+}
+document.write('<script src="./separate-links.js?v=10250"></script>');
