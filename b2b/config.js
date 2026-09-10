0ca6df7dd99cf86435c3e383d7003e90fed86573
nativeelaneeru <ir117737@gmail.com>
2026-09-10 13:44:08 +0530

    Wire B2B IndexedDB into app v9.7

--- b2b/config.js
+++ b2b/config.js
@@ -1,11 +1,12 @@
 window.NEL_B2B_CONFIG={
   apiUrl:'https://script.google.com/macros/s/AKfycbx2s0l5A8LAdD1j24395XJSTMd5cEU7QdUkTI8LarDzatF-vVw6ODfm5x7MVJUkP9aB/exec',
-  appVersion:'9.6.2',
+  appVersion:'9.7.0',
   supportPhone:'7411807675',
   supportWhatsApp:'917411807675',
   company:'Sri Govindadri Ventures'
 };
-if(!window.NEL_B2B_UI_V113) document.write('<script src="./ui-v113.js?v=962"></script>');
-document.write('<script src="./separate-links.js?v=962"></script>');
-document.write('<script src="./session-v960.js?v=962"></script>');
-document.write('<script src="./update-notifier.js?v=962"></script>');
+document.write('<script src="./idb-v1.js?v=970"></script>');
+if(!window.NEL_B2B_UI_V113) document.write('<script src="./ui-v113.js?v=970"></script>');
+document.write('<script src="./separate-links.js?v=970"></script>');
+document.write('<script src="./session-v960.js?v=970"></script>');
+document.write('<script src="./update-notifier.js?v=970"></script>');
