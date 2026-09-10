fd5458cdc7ce61e78f8145590af23f6e39268196
nativeelaneeru <ir117737@gmail.com>
2026-09-10 16:19:18 +0530

    Enable B2C subscriptions and Refer & Earn UI

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.32.0-pwa',
+  appVersion: '10.33.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
@@ -12,14 +12,15 @@ window.NEL_CONFIG = {
 
 window.NEL_RPC_V109 = true;
 
-document.write('<script src="./idb-v1.js?v=10320"></script>');
-if (!window.NEL_UI_V110) document.write('<script src="./ui-v110.js?v=10320"></script>');
-if (!window.NEL_UI_V111) document.write('<script src="./ui-v111.js?v=10320"></script>');
-if (!window.NEL_UI_V112) document.write('<script src="./ui-v112.js?v=10320"></script>');
-if (!window.NEL_UI_V113) document.write('<script src="./ui-v113.js?v=10320"></script>');
-if (!window.NEL_UI_V125) document.write('<script src="./ui-v125.js?v=10320"></script>');
-if (!window.NEL_UI_V127) document.write('<script src="./ui-v127.js?v=10320"></script>');
-document.write('<script src="./order-sync-v1.js?v=10320"></script>');
-document.write('<script src="./separate-links.js?v=10320"></script>');
-document.write('<script src="./cart-visibility-v10320.js?v=10320"></script>');
-document.write('<script src="./update-notifier.js?v=10320"></script>');
+document.write('<script src="./idb-v1.js?v=10330"></script>');
+if (!window.NEL_UI_V110) document.write('<script src="./ui-v110.js?v=10330"></script>');
+if (!window.NEL_UI_V111) document.write('<script src="./ui-v111.js?v=10330"></script>');
+if (!window.NEL_UI_V112) document.write('<script src="./ui-v112.js?v=10330"></script>');
+if (!window.NEL_UI_V113) document.write('<script src="./ui-v113.js?v=10330"></script>');
+if (!window.NEL_UI_V125) document.write('<script src="./ui-v125.js?v=10330"></script>');
+if (!window.NEL_UI_V127) document.write('<script src="./ui-v127.js?v=10330"></script>');
+document.write('<script src="./order-sync-v1.js?v=10330"></script>');
+document.write('<script src="./separate-links.js?v=10330"></script>');
+document.write('<script src="./cart-visibility-v10320.js?v=10330"></script>');
+document.write('<script src="./customer-growth-v10330.js?v=10330"></script>');
+document.write('<script src="./update-notifier.js?v=10330"></script>');
