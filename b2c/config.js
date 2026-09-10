dffea422387d9eb00dcc62d8dcb121ae5ce8dc48
nativeelaneeru <ir117737@gmail.com>
2026-09-10 12:30:23 +0530

    Load direct checkout v126 and bump B2C to 10.27

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.26.0-pwa',
+  appVersion: '10.27.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
@@ -12,10 +12,11 @@ window.NEL_CONFIG = {
 
 window.NEL_RPC_V109 = true;
 
-if (!window.NEL_UI_V110) document.write('<script src="./ui-v110.js?v=10260"></script>');
-if (!window.NEL_UI_V111) document.write('<script src="./ui-v111.js?v=10260"></script>');
-if (!window.NEL_UI_V112) document.write('<script src="./ui-v112.js?v=10260"></script>');
-if (!window.NEL_UI_V113) document.write('<script src="./ui-v113.js?v=10260"></script>');
-if (!window.NEL_UI_V125) document.write('<script src="./ui-v125.js?v=10260"></script>');
-document.write('<script src="./separate-links.js?v=10260"></script>');
-document.write('<script src="./update-notifier.js?v=10260"></script>');
+if (!window.NEL_UI_V110) document.write('<script src="./ui-v110.js?v=10270"></script>');
+if (!window.NEL_UI_V111) document.write('<script src="./ui-v111.js?v=10270"></script>');
+if (!window.NEL_UI_V112) document.write('<script src="./ui-v112.js?v=10270"></script>');
+if (!window.NEL_UI_V113) document.write('<script src="./ui-v113.js?v=10270"></script>');
+if (!window.NEL_UI_V125) document.write('<script src="./ui-v125.js?v=10270"></script>');
+if (!window.NEL_UI_V126) document.write('<script src="./ui-v126.js?v=10270"></script>');
+document.write('<script src="./separate-links.js?v=10270"></script>');
+document.write('<script src="./update-notifier.js?v=10270"></script>');
