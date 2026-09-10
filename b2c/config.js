419d59f7a0332a08c6c575e8ee61cf5fecbc1499
nativeelaneeru <ir117737@gmail.com>
2026-09-10 13:03:44 +0530

    Release B2C 10.28 resilient checkout

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.27.0-pwa',
+  appVersion: '10.28.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
@@ -12,11 +12,12 @@ window.NEL_CONFIG = {
 
 window.NEL_RPC_V109 = true;
 
-if (!window.NEL_UI_V110) document.write('<script src="./ui-v110.js?v=10270"></script>');
-if (!window.NEL_UI_V111) document.write('<script src="./ui-v111.js?v=10270"></script>');
-if (!window.NEL_UI_V112) document.write('<script src="./ui-v112.js?v=10270"></script>');
-if (!window.NEL_UI_V113) document.write('<script src="./ui-v113.js?v=10270"></script>');
-if (!window.NEL_UI_V125) document.write('<script src="./ui-v125.js?v=10270"></script>');
-if (!window.NEL_UI_V126) document.write('<script src="./ui-v126.js?v=10270"></script>');
-document.write('<script src="./separate-links.js?v=10270"></script>');
-document.write('<script src="./update-notifier.js?v=10270"></script>');
+if (!window.NEL_UI_V110) document.write('<script src="./ui-v110.js?v=10280"></script>');
+if (!window.NEL_UI_V111) document.write('<script src="./ui-v111.js?v=10280"></script>');
+if (!window.NEL_UI_V112) document.write('<script src="./ui-v112.js?v=10280"></script>');
+if (!window.NEL_UI_V113) document.write('<script src="./ui-v113.js?v=10280"></script>');
+if (!window.NEL_UI_V125) document.write('<script src="./ui-v125.js?v=10280"></script>');
+if (!window.NEL_UI_V126) document.write('<script src="./ui-v126.js?v=10280"></script>');
+if (!window.NEL_UI_V127) document.write('<script src="./ui-v127.js?v=10280"></script>');
+document.write('<script src="./separate-links.js?v=10280"></script>');
+document.write('<script src="./update-notifier.js?v=10280"></script>');
