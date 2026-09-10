6668bc34f8736484f0787cb007c4dc35647acf34
nativeelaneeru <ir117737@gmail.com>
2026-09-10 11:39:05 +0530

    Version mature B2C PWA for separated deployment

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.22.0-pwa',
+  appVersion: '10.23.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
   parentCompany: 'Sri Govindadri Ventures',
@@ -16,14 +16,15 @@ window.NEL_CONFIG = {
 window.NEL_RPC_V109 = true;
 
 if (!window.NEL_UI_V110) {
-  document.write('<script src="./ui-v110.js?v=10220"></script>');
+  document.write('<script src="./ui-v110.js?v=10230"></script>');
 }
 if (!window.NEL_UI_V111) {
-  document.write('<script src="./ui-v111.js?v=10220"></script>');
+  document.write('<script src="./ui-v111.js?v=10230"></script>');
 }
 if (!window.NEL_UI_V112) {
-  document.write('<script src="./ui-v112.js?v=10220"></script>');
+  document.write('<script src="./ui-v112.js?v=10230"></script>');
 }
 if (!window.NEL_UI_V113) {
-  document.write('<script src="./ui-v113.js?v=10220"></script>');
+  document.write('<script src="./ui-v113.js?v=10230"></script>');
 }
+document.write('<script src="./separate-links.js?v=10230"></script>');
