482207b3aed897dbc4150c341a5d23a233591b90
nativeelaneeru <ir117737@gmail.com>
2026-09-10 12:17:03 +0530

    Bump B2B and load update notifier

--- b2b/config.js
+++ b2b/config.js
@@ -1,12 +1,11 @@
 window.NEL_B2B_CONFIG={
   apiUrl:'https://script.google.com/macros/s/AKfycbx2s0l5A8LAdD1j24395XJSTMd5cEU7QdUkTI8LarDzatF-vVw6ODfm5x7MVJUkP9aB/exec',
-  appVersion:'9.6.1',
+  appVersion:'9.6.2',
   supportPhone:'7411807675',
   supportWhatsApp:'917411807675',
   company:'Sri Govindadri Ventures'
 };
-if(!window.NEL_B2B_UI_V113){
-  document.write('<script src="./ui-v113.js?v=961"></script>');
-}
-document.write('<script src="./separate-links.js?v=961"></script>');
-document.write('<script src="./session-v960.js?v=961"></script>');
+if(!window.NEL_B2B_UI_V113) document.write('<script src="./ui-v113.js?v=962"></script>');
+document.write('<script src="./separate-links.js?v=962"></script>');
+document.write('<script src="./session-v960.js?v=962"></script>');
+document.write('<script src="./update-notifier.js?v=962"></script>');
