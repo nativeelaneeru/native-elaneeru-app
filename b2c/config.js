e187a8954ca1a27b9432dc1db4ceb2027f64f574
nativeelaneeru <ir117737@gmail.com>
2026-09-15 15:47:50 +0530

    Load B2C coming soon banner

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.41.0-pwa',
+  appVersion: '10.42.0-pwa',
   paymentRelease: '10.35.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
@@ -33,3 +33,4 @@ document.write('<script src="./referral-reward-v10340.js?v=10360"></script>');
 document.write('<script src="./payment-upi-v10350.js?v=10350"></script>');
 document.write('<script src="./update-notifier.js?v=10360"></script>');
 document.write('<script src="./install-analytics-v10420.js?v=10420"></script>');
+document.write('<script src="./coming-soon-banner-v10430.js?v=10430"></script>');
