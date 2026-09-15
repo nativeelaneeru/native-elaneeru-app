5aeb388147d3bf36320a700f09cc05bb0c8dc7d0
nativeelaneeru <ir117737@gmail.com>
2026-09-15 15:56:37 +0530

    Refresh B2C cache for launch status banner

--- b2c/sw.js
+++ b2c/sw.js
@@ -1,4 +1,4 @@
-const CACHE='native-elaneeru-b2c-v10.51.0';
+const CACHE='native-elaneeru-b2c-v10.52.0';
 const SHELL=[
   './','./index.html','./login/index.html','./config.js','./manifest.webmanifest','./idb-v1.js','./order-sync-v1.js',
   './boot-recovery-v10360.js','./image-fallback-v918.js','./product-images-v10380.js','./ui-v108.js','./ui-v110.js','./ui-v111.js','./ui-v112.js','./ui-v113.js','./ui-v125.js','./ui-v127.js',
