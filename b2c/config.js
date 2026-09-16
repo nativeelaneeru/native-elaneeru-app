51a6dc0e098d5c01fc4870d61d13746246189b28
nativeelaneeru <ir117737@gmail.com>
2026-09-16 16:56:39 +0530

    Load B2C pricing hierarchy v10.58.1

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.58.0-pwa',
+  appVersion: '10.58.1-pwa',
   paymentRelease: '10.35.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
@@ -41,3 +41,4 @@ document.write('<script src="./install-analytics-v10420.js?v=10420"></script>');
 document.write('<script src="./coming-soon-banner-v10430.js?v=10431"></script>');
 document.write('<script src="./live-pricing-v10440.js?v=10441"></script>');
 document.write('<script src="./base-price-v10450.js?v=10450"></script>');
+document.write('<script src="./pricing-hierarchy-v10581.js?v=10581"></script>');
