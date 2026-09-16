1d6d3cb9b5f01f0f14805ad3c5c3a06c7f91b39c
nativeelaneeru <ir117737@gmail.com>
2026-09-16 13:02:07 +0530

    Load loop-safe B2C UX polish

--- b2c/config.js
+++ b2c/config.js
@@ -1,6 +1,6 @@
 window.NEL_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbwWt1gjknt21us91HbFVbFdr5DbkEta-ZmETke-axuepZYp8fQRwAiGTnlgiKrxthb3/exec',
-  appVersion: '10.57.1-pwa',
+  appVersion: '10.57.2-pwa',
   paymentRelease: '10.35.0-pwa',
   supportPhone: '7411807675',
   supportWhatsApp: '917411807675',
@@ -30,7 +30,7 @@ document.write('<script src="./separate-links.js?v=10350"></script>');
 document.write('<script src="./cart-visibility-v10320.js?v=10350"></script>');
 document.write('<script src="./customer-growth-v10330.js?v=10350"></script>');
 document.write('<script src="./subscription-cashback-v10540.js?v=10540"></script>');
-document.write('<script src="./ux-polish-v10560.js?v=10560"></script>');
+document.write('<script src="./ux-polish-v10561.js?v=10561"></script>');
 document.write('<script src="./referral-reward-v10340.js?v=10360"></script>');
 document.write('<script src="./payment-upi-v10350.js?v=10350"></script>');
 document.write('<script src="./update-notifier.js?v=10360"></script>');
